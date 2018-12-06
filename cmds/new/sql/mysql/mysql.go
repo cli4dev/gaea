0833d3ddbd87465eaadb329ece2125242c23a4ad
yanglei <yanglei@100bm.cn>
2018-12-06 15:46:24 +0800

    .

--- cmds/new/sql/mysql/mysql.go
+++ cmds/new/sql/mysql/mysql.go
@@ -1,180 +1,96 @@
 package mysql
 
 import (
-	"bytes"
+	"database/sql"
 	"fmt"
-	"strings"
-	"text/template"
+	"os"
+	"path/filepath"
 
-	"github.com/micro-plat/gaea/cmds/new/sql"
-
-	"github.com/micro-plat/gaea/cmds/new/util/conf"
+	"github.com/micro-plat/gaea/cmds"
+	"github.com/micro-plat/gaea/cmds/new/sql/mysql"
+	"github.com/micro-plat/gaea/cmds/new/util/md"
+	"github.com/urfave/cli"
 )
 
-func translate(c string, input interface{}) (string, error) {
-	var tmpl = template.New("mysql").Funcs(makeFunc())
-	np, err := tmpl.Parse(c)
-	if err != nil {
-		return "", err
-	}
-	buff := bytes.NewBufferString("")
-	if err := np.Execute(buff, input); err != nil {
-		return "", err
-	}
-	return buff.String(), nil
-}
-func getNull(n bool) string {
-	if n {
-		return "not null"
-	}
-	return ""
-}
-func getDef(n string) string {
-	if n == "" {
-		return ""
-	}
-	if strings.TrimSpace(n) == "-" {
-		return "default '-'"
-	}
-	return "default " + n
+type mysqlCmd struct {
 }
 
-func getUnqs(tb *conf.Table) (out []map[string]interface{}) {
-	out = make([]map[string]interface{}, 0, 1)
-	seqs := make([]string, 0, 1)
-	for i, v := range tb.Cons {
-		if strings.Contains(v, "UNQ") {
-			seqs = append(seqs, tb.CNames[i])
-		}
+func NewMysqlCmd() cli.Command {
+	p := &mysqlCmd{}
+	return cli.Command{
+		Name:   "mysql",
+		Usage:  "创建SQL语句",
+		Flags:  p.geStartFlags(),
+		Action: p.action,
 	}
-	if len(seqs) == 0 {
+}
+func (p *mysqlCmd) geStartFlags() []cli.Flag {
+	flags := make([]cli.Flag, 0, 0)
+	return flags
+}
+func (p *mysqlCmd) action(c *cli.Context) (err error) {
+	if c.NArg() < 2 {
+		err = fmt.Errorf("请指定md文件路径和输出路径")
+		fmt.Println(err)
+		cli.ShowCommandHelp(c, c.Command.Name)
+		return err
+	}
+	filePath := c.Args().First()
+	outPath := c.Args().Get(1)
+	tables, err := md.Markdown2Table(filePath)
+	if err != nil {
+		cmds.Log.Error(err)
+		return err
+	}
+	tmpls, err := mysql.GetTmples(tables)
+	if err != nil {
+		cmds.Log.Error(err)
+		return err
+	}
+	if len(tmpls) == 0 {
+		cmds.Log.Errorf("%s中未找到数据表信息", filePath)
 		return nil
 	}
-	out = append(out, map[string]interface{}{
-		"name": fmt.Sprintf("unq_%s_%s", tb.Name, seqs[0]),
-		"flds": strings.Join(seqs, ","),
-	})
-	return out
+	if err = p.createFile(outPath, tmpls); err != nil {
+		cmds.Log.Error(err)
+		return err
+	}
+	cmds.Log.Infof("SQL生成完成,共生成%d个文件", len(tmpls))
+	return nil
 }
 
-func getPks(tb *conf.Table) []string {
-	out := make([]string, 0, 1)
-	for i, v := range tb.Cons {
-		if strings.Contains(v, "PK") {
-			out = append(out, tb.CNames[i])
-		}
-	}
-	return out
-}
-func getSeqs(tb *conf.Table) (out []map[string]interface{}) {
-	out = make([]map[string]interface{}, 0, 1)
-	seqs := make(map[string]string)
-	for i, v := range tb.Cons {
-		if strings.Contains(v, "SEQ") {
-			seqs[tb.CNames[i]] = tb.CNames[i]
-		}
-	}
-	for _, v := range seqs {
-		out = append(out, map[string]interface{}{
-			"name": fmt.Sprintf("seq_%s_%s", fGetNName(tb.Name), getFilterName(tb.Name, v)),
-			"min":  100,
-			"max":  99999999999,
-		})
-	}
-	return out
-}
-
-func GetTmples(tbs []*conf.Table) (out map[string]string, err error) {
-	out = make(map[string]string, len(tbs))
-	for _, tb := range tbs {
-		columns := make([]map[string]interface{}, 0, len(tb.CNames))
-		for i, v := range tb.CNames {
-			row := map[string]interface{}{
-				"name": v,
-				"desc": strings.Replace(tb.Descs[i], ";", " ", -1),
-				"type": tb.Types[i],
-				"len":  tb.Lens[i],
-				"def":  getDef(tb.Defs[i]),
-				"null": getNull(tb.IsNulls[i]),
-				"end":  i != len(tb.CNames)-1,
+func (p *mysqlCmd) createFile(root string, data map[string]string) error {
+	for k, v := range data {
+		path := filepath.Join(root, k)
+		dir := filepath.Dir(path)
+		_, err := os.Stat(dir)
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(dir, os.ModeDir|os.ModePerm)
+			if err != nil {
+				err = fmt.Errorf("创建文件夹%s失败:%v", path, err)
+				return err
 			}
-			columns = append(columns, row)
 		}
-		input := map[string]interface{}{
-			"name":    tb.Name,
-			"desc":    tb.Desc,
-			"columns": columns,
-			"seqs":    getSeqs(tb),
-			"pks":     getPks(tb),
-			"unqs":    getUnqs(tb),
+		if _, err := os.Stat(path); err == nil || os.IsExist(err) {
+			cmds.Log.Warn("文件已存在:", path)
+			continue
 		}
-		out[fmt.Sprintf("%s.sql", tb.Name)], err = translate(tableTmpl, input)
+		srcf, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, os.ModeAppend|os.ModePerm)
 		if err != nil {
-			return nil, err
+			err = fmt.Errorf("无法打开文件:%s(err:%v)", path, err)
+			return err
 		}
-	}
-	return out, nil
-}
-
-func makeFunc() map[string]interface{} {
-	return map[string]interface{}{
-		"cName": fGetCName,
-		"nName": fGetNName,
-		"cType": fGetType,
-	}
-}
-func fGetCName(n string) string {
-	items := strings.Split(n, "_")
-	nitems := make([]string, 0, len(items))
-	for _, i := range items {
-		nitems = append(nitems, strings.ToUpper(i[0:1])+i[1:])
-	}
-	return strings.Join(nitems, "")
-}
-func fGetNName(n string) string {
-	items := strings.Split(n, "_")
-	if len(items) <= 1 {
-		return n
-	}
-	return strings.Join(items[1:], "_")
-}
-
-func fGetType(n string) string {
-	if strings.HasPrefix(n, "nvarchar") {
-		return "string"
-	} else if strings.HasPrefix(n, "number") {
-		if strings.Contains(n, ",") {
-			return "float64"
+		defer srcf.Close()
+		_, err = srcf.WriteString(v)
+		if err != nil {
+			return err
 		}
-		return "int64"
-	} else if strings.HasPrefix(n, "date") {
-		return "time.Time"
+		cmds.Log.Info("生成文件:", path)
 	}
-	return "string"
-}
-
-func getFilterName(t string, f string) string {
-	text := make([]string, 0, 1)
-	tb := strings.Split(t, "_")
-	fs := strings.Split(f, "_")
-	for _, v := range fs {
-		ex := false
-		for _, k := range tb {
-			if v == k {
-				ex = true
-				break
-			}
-		}
-		if !ex {
-			text = append(text, v)
-		}
-	}
-	if len(text) == 0 {
-		return "id"
-	}
-	return strings.Join(text, "_")
+	return nil
 
 }
+
 func init() {
 	sql.Register("mysql", GetTmples)
 }
