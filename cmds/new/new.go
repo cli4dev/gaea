9dcbe18e6e2aa295ed5ad68f28d1f13bee7001dd
yanglei <yanglei@100bm.cn>
2018-12-07 15:02:12 +0800

    修改project创建命令

--- cmds/new/new.go
+++ cmds/new/new.go
@@ -2,11 +2,8 @@ package new
 
 import (
 	"github.com/micro-plat/gaea/cmds"
-	"github.com/micro-plat/gaea/cmds/new/html"
-	"github.com/micro-plat/gaea/cmds/new/key"
 	"github.com/micro-plat/gaea/cmds/new/module"
 	"github.com/micro-plat/gaea/cmds/new/project"
-	"github.com/micro-plat/gaea/cmds/new/service"
 	"github.com/micro-plat/gaea/cmds/new/sql"
 	_ "github.com/micro-plat/gaea/cmds/new/sql/mysql"
 	_ "github.com/micro-plat/gaea/cmds/new/sql/oracle"
@@ -18,24 +15,28 @@ func init() {
 }
 
 func getCmd() []cli.Command {
-	return []cli.Command{
-		cli.Command{
-			Name:  "new",
-			Usage: "创建项目,文件，数据库等",
-			Subcommands: []cli.Command{
-				project.NewProjectCmd(),
-				module.NewModuleCmd(),
-				service.NewServiceCmd(),
-				sql.NewSqlCmd(),
-				key.NewKeyCmd(),
-				html.NewHTMLCmd(),
-				//sql.NewStructCmd(),
-			},
-			cli.Command{
-			Name:  "project",
-			Usage: "创建项目,文件，数据库等",
-			Subcommands: []cli.Command{
-				project.NewProjectCmd(),
-			},
-		}}
+	cmds := make([]cli.Command, 0, 4)
+	cmds = append(cmds, sql.NewSqlCmd())
+	cmds = append(cmds, module.NewModuleCmd())
+	cmds = append(cmds, project.NewProjectCmd()...)
+	return cmds
+	// return []cli.Command{
+	// 	cli.Command{
+	// 		Name:  "new",
+	// 		Usage: "创建项目,文件，数据库等",
+	// 		Subcommands: []cli.Command{
+	// 			project.NewProjectCmd(),
+	// 			module.NewModuleCmd(),
+	// 			service.NewServiceCmd(),
+	// 			sql.NewSqlCmd(),
+	// 			key.NewKeyCmd(),
+	// 			html.NewHTMLCmd(),
+	// 			//sql.NewStructCmd(),
+	// 		},
+	// 		cli.Command{
+	// 		Name:  "project",
+	// 		Usage: "创建项目,文件，数据库等",
+	// 		Subcommands: project.NewProjectCmd(),
+	// 		,
+	// 	}}
 }
