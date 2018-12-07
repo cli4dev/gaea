dd90b8130e9fdbea9242bd0e25fd2828a79e266f
yanglei <yanglei@100bm.cn>
2018-12-07 15:49:42 +0800

    修改sql生成方式

--- cmds/new/new.go
+++ cmds/new/new.go
@@ -3,23 +3,8 @@ package new
 import (
 	"github.com/micro-plat/gaea/cmds"
 	"github.com/micro-plat/gaea/cmds/new/module"
-	"github.com/micro-plat/gaea/cmds/new/sql"
-	_ "github.com/micro-plat/gaea/cmds/new/sql/mysql"
-	_ "github.com/micro-plat/gaea/cmds/new/sql/oracle"
-	"github.com/urfave/cli"
 )
 
 func init() {
-	cmds.Registry(getCmd()...)
-}
-
-func getCmd() []cli.Command {
-	cmds := make([]cli.Command, 0, 4)
-	cmds = append(cmds, module.NewModuleCmd())
-	cmds = append(cmds, cli.Command{
-		Name:        "create",
-		Usage:       "创建数据库表结构",
-		Subcommands: sql.NewSqlCmd(),
-	})
-	return cmds
+	cmds.Registry(module.NewModuleCmd())
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/micro-plat/gaea/cmds"
 import _ "github.com/micro-plat/gaea/cmds/new"
+import _ "github.com/micro-plat/gaea/cmds/create"
 
 func main() {
 	cmds.Start()
